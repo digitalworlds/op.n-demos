8ef69e1ad47e6c70954522442508bfc5d69e75ce
Digital Worlds Institute <angelos@digitalworlds.ufl.edu>
2023-01-25 16:09:28 -0500

    Update main.js

--- AppTemplate/main.js
+++ AppTemplate/main.js
@@ -10,33 +10,33 @@ var main=function(args){
 	var wind=args.app.getWindow();//gets the window object, which has many parameters, methods, and listeners
 	//for example:
 	wind.whenResized().then((wind)=>{
-			console.log("Window resized!");
+		area.innerHTML="Window resized!";
 	})
 
-
-	var menubar;
+	//We create a GUI menu bar with one menu item:
 	var menulayout=new MenuLayout();
-	menubar=menulayout.getMenuBar();
-	menubar.append(new MenuItem('Help')).whenClicked().then((item)=>{
-		console.log("Help clicked");
+	menulayout.getMenuBar().append(new MenuItem('Help')).whenClicked().then((item)=>{
+		area.innerHTML="Help clicked!";
 	});
 
 	//You can append GUI elements to the window like this:
 	wind.getContent().append(menulayout);
 
-	//Or alternatively you can append custom divs made with vanilla JS
+	//Or alternatively you can append custom divs made with vanilla JS:
 	var area=document.createElement('div');
 	opn.set(area.style,{
 		position:'absolute',
-			display:'block',
-			width:'100%',
-			height:'100%',
-			background:'linear-gradient(to bottom,blue 0%, white 100%)',
-			overflow:'hidden',
-			color: 'white',
-			fontWeight:'900',
-			fontSize:'28px'
-	})
+		display:'block',
+		width:'100%',
+		height:'100%',
+		padding:'10px',
+		background:'linear-gradient(to bottom,blue 0%, white 100%)',
+		overflow:'hidden',
+		color: 'white',
+		fontWeight:'700',
+		fontSize:'28px',
+		fontFamily:'Arial'
+	});
 	menulayout.getContainer().div.appendChild(area);
 	area.innerHTML="Hello World!";
 
